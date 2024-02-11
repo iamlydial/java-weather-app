1b32a9f47d8d1bcb328dbf8c473fec752502e13d
iamlydial <lydia.lavecchia@gmail.com>
2024-02-11 20:00:55 +0100

    GUI Component Addition and Image Handling
    Description:
    - Implemented the addGuiComponents() method to add search-related components to the GUI, including a JTextField for search input and a JButton for search functionality.
    - Positioned the search field and button within the GUI, setting their bounds and adjusting the font style and size of the search field.
    - Created a loadImage() method to load images from a specified resource path, using ImageIO to read the image file and returning an ImageIcon for GUI component rendering.
    - Utilized the loadImage() method to set the search button's icon with an image loaded from the "src/assets/search.png" path.
    - Configured the search button to change the cursor to a hand cursor (Cursor.HAND_CURSOR) when hovering over it.
    
    This commit enhances the WeatherApp GUI by adding essential search-related components and introducing a method for handling image resources, improving the overall user interface and functionality.

--- java-weather-app/src/WeatherApp.java
+++ java-weather-app/src/WeatherApp.java
@@ -1,4 +1,9 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class WeatherApp extends JFrame {
     // setup gui and add a title
@@ -20,5 +25,50 @@ public class WeatherApp extends JFrame {
         // prevent any resize of the GUI
         setResizable(false);
 
+        addGuiComponents();
+
+
+
+    }
+
+    private void addGuiComponents(){
+        // search field
+        JTextField searchTextField =new JTextField();
+
+        // set the location and size of our component
+        searchTextField.setBounds(15, 15, 351, 45);
+
+        // change the font style and size
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+
+        add(searchTextField);
+
+        //search button
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+
+        //change icon to hand cursor when hovering over this button
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        add(searchButton);
+
+        //weather image
+        JLabel weatherConditionImage = new JLabel(loadImage("src/assets/cloudy.png"));
+        weatherConditionImage.setBounds(0,125, 450, 217);
+        add(weatherConditionImage);
+    }
+
+    // used to create images on GUI component
+    private ImageIcon loadImage(String resourcePath){
+        try{
+            // read image of the file from path given
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+
+            //returns an image icon so that our component and render it
+            return new ImageIcon(image);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        System.out.println("Could not find resource");
+        return null;
     }
 }
